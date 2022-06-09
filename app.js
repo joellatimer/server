7ec4e2fd4bd879acc1250fc6ee6e52573368ebd4
Joel Latimer <joellatimer@outlook.com>
2022-06-09 09:21:08 -0400

    Cors Again

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods:GET, POST, PATCH, PUT, DELETE, OPTIONS")
+    res.header("Access-control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
   });
 
 app.use(morgan('dev'))
