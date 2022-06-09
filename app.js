d4850b930cc1e588ae3274182cfcf11394651016
Joel Latimer <joellatimer@outlook.com>
2022-06-09 10:09:37 -0400

    Cors s

--- app.js
+++ app.js
@@ -8,15 +8,25 @@ const memberRoutes = require('./routes/memberRoutes')
 const meetingRoutes = require('./routes/meetingRoutes')
 const groupMemberRoutes = require('./routes/groupMemberRoutes')
 const attendsRoutes = require('./routes/attendsRoutes')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
 
 
-app.use(
-    cors({
-        origin: "http://162.243.173.105:81",
-        methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'),
+    res.header('Access-Control-Allow-Headers', '*')
+
+    
+    if (req.method === 'OPTIONS') {
+    res.header('Aaccess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
+    }
+    next();
+})
+  
+    
         
-    })    
-)        
+ 
 
 app.use(morgan('dev'))
 
